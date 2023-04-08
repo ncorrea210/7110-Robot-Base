bcea28a2a398a3210c41b9c0b2e797fd9ab2fa15
Nathan Correa <correanathanh@gmail.com>
2023-04-08 18:59:07 -0400

    documentation for hb::Subsystem

--- src/main/include/utils/Subsystem.h
+++ src/main/include/utils/Subsystem.h
@@ -8,9 +8,17 @@ namespace hb {
   class Subsystem : public frc2::SubsystemBase {
     public:
 
+      /**
+       * @brief Includes a map of labels and values for any item to be put on shuffleboard
+       * 
+       * @returns unodered_map of the label and value for any item
+      */
       virtual std::unordered_map<std::string, double> GetTelemetry() = 0;
 
-      virtual void UpdateTelemetry();
+      /**
+       * @brief function that should be used to actually place values into map
+      */
+      virtual void UpdateTelemetry() = 0;
 
     protected:
       explicit Subsystem();
