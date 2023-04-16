c8e9aa0736a70c2a615e2e758f5fa70ae9c6b1ea
Nathan Correa <correanathanh@gmail.com>
2023-04-15 22:00:39 -0400

    removed extraneous constants

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -47,17 +47,15 @@ namespace DriveConstants {
     const int kPidgeonID = 0;
     } // namespace CanIds
 
-constexpr double kFrontLeftOffset = 41.31  /*-138.25*/ /**-142.03**/; //encoder 2
-constexpr double kRearLeftOffset =  -42.71 /*-42.71*/ /**20.83**/; //encoder 1
-constexpr double kFrontRightOffset = 107.48 /*102.66*/ /*109.6**/; //encoder 3
-constexpr double kRearRightOffset = -168.75 /*-162.25*//**21.09*/; //encoder 4
+constexpr double kFrontLeftOffset = 41.31; //encoder 2
+constexpr double kRearLeftOffset =  -42.71; //encoder 1
+constexpr double kFrontRightOffset = 107.48; //encoder 3
+constexpr double kRearRightOffset = -168.75; //encoder 4
 
 constexpr auto kMaxSpeed = 4_mps;
-constexpr auto kFineSpeed = 1_mps;
 constexpr auto kMaxAngularSpeed = units::radians_per_second_t(2 * std::numbers::pi);
 constexpr auto kMaxAngularAcceleration = units::radians_per_second_squared_t(2 * std::numbers::pi);
 
-constexpr auto kPushnBalanceSpeed = 2_mps;
 
 }  // namespace DriveConstants
 
